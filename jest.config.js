4b8dda9a7d3fed1ab8ea0a7d034ee3fcc807d78b
Jan Kaul <jankaul@mailbox.org>
2022-03-12 11:01:52 +0100

    add fileMock

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
     testEnvironment: "jsdom",
     moduleNameMapper: {
         "@fontsource/*": "<rootDir>/__mocks__/font-mock.js",
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
         // other rules...
     },
 };
